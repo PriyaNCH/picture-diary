068ec20288cae7de384bf8d1e194714fd61f1da3
Vishnu Priya Nallan Chakravarthula <VishnuPriya.NallanChakravarthula@jda.com>
2017-10-22 18:49:39 +0530

    Added code to select images from a folder sorted by month and add/submit buttons in detail view

---
+++ app/src/main/java/com/lilac/priyacoder/materialdesinginkotlin/MonthComparator.kt
@@ -0,0 +1,19 @@
+package com.lilac.priyacoder.materialdesinginkotlin
+
+/**
+ * Created by 1021422 on 10/15/2017.
+ */
+class MonthComparator {
+
+
+    companion object : Comparator<String> {
+
+        val months =  arrayOf("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+
+        override fun compare(month1: String, month2: String): Int = when {
+
+            months.indexOf(month1) == months.indexOf(month2) -> 0
+            else -> months.indexOf(month1) - months.indexOf(month2)
+        }
+    }
+}
---
+++ app/src/main/java/com/lilac/priyacoder/materialdesinginkotlin/MonthThumbnail.kt
@@ -0,0 +1,12 @@
+package com.lilac.priyacoder.materialdesinginkotlin
+
+import android.content.Context
+
+/**
+ * Created by 1021422 on 10/10/2017.
+ */
+class MonthThumbnail(val name: String,  val imagePath: String, val isFav: Boolean = false) {
+//    fun getImageResourceId(context: Context): Int {
+//        return context.resources.getIdentifier(this.imagePath, "drawable", context.packageName)
+//    }
+}
